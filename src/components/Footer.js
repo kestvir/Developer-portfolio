fd1bdb6353ad7817bfdf750514962f24dee4549c
kestvir <v.kestutis123@gmail.com>
2020-07-03 14:15:46 +0300

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 
 import React from "react"
+import { Link } from "gatsby"
 import SocialLinks from "../constants/social-links"
+
 const Footer = () => {
     return (
         <footer className="footer">
@@ -10,6 +12,10 @@ const Footer = () => {
                     copyright&copy;{new Date().getFullYear()}
                     <span> Kestutis Virbickas </span> all rights reserved
                 </h4>
+                <h5>
+                    Website source
+                    <Link to="https://github.com/kestvir/Developer-portfolio" target="_blank" rel="noreferrer">code</Link>
+                </h5>
             </div>
         </footer>
     )
