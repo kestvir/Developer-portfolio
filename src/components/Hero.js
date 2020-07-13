432f1c9bf5b771383542910e1a2166ef598cae5c
kestvir <v.kestutis123@gmail.com>
2020-07-13 13:08:32 +0300

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const query = graphql`
   {
     file(relativePath: { eq: "hero.jpg" }) {
       childImageSharp {
-        fluid(quality: 100, maxWidth: 2400px) {
+        fluid(quality: 100, maxWidth: 2400) {
             ...GatsbyImageSharpFluid
           }
       }
