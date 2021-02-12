c1edf40d02c7e6c2470689d0a78485291927d411
kestutisvirbickas <v.kestutis123@gmail.com>
2021-02-12 11:26:17 +0200

    added required prop to form inputs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import Title from './Title'
+import React from "react"
+import Title from "./Title"
 
 const Contact = () => {
-    return (
-        <section className="section contact" id="contact-section">
-            <Title title={"Contact"} />
-            <article className="contact-form">
-                <h3>get in touch</h3>
-                <form action={`https://formspree.io/mknqvbve`} method="POST">
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="name"
-                            className="form-control"
-                        />
-                        <input
-                            type="email"
-                            placeholder="email"
-                            name="email"
-                            className="form-control"
-                        />
-                        <textarea
-                            name="message"
-                            rows="5"
-                            placeholder="message"
-                            className="form-control"
-                        ></textarea>
-                    </div>
-                    <button type="submit" className="submit-btn btn">
-                        submit here
-                    </button>
-                </form>
-            </article>
-        </section>
-    )
+  return (
+    <section className="section contact" id="contact-section">
+      <Title title={"Contact"} />
+      <article className="contact-form">
+        <h3>get in touch</h3>
+        <form action={`https://formspree.io/mknqvbve`} method="POST">
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              className="form-control"
+              required
+            />
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              className="form-control"
+              required
+            />
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="message"
+              className="form-control"
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className="submit-btn btn">
+            submit here
+          </button>
+        </form>
+      </article>
+    </section>
+  )
 }
 
-export default Contact;
+export default Contact
